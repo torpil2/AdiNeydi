df007878e7930e7a07371b83c9aab2938fbb36cf
torpil2 <bugraksener@gmail.com>
2023-03-14 23:45:01 +0300

    first release

---
+++ AdiNeydiProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AdiNeydiProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
